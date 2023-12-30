84e2bfdae124ac3bc50e42e5583eb2e0ca49f0ea
Mukti18p <155235925+Mukti18p@users.noreply.github.com>
2023-12-30 21:31:26 +0530

    Add files via upload

---
+++ loop/exp2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    float ft,ct;
+
+
+
+    printf("The temp in farenhit is :");
+    scanf("%f\n",&ft);
+    printf("The temp in ces is :%f\n",ct=(ft-32)/1.8);
+
+
+     return 0;
+
+}
